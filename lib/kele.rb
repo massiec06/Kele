09357ba8c2d59bf07d770be6c7d5d3300a29d34b
Chris Massie <massiec06@gmail.com>
2017-10-19 18:16:22 -0400

    checkpoint 5 error in terminal

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,10 @@
 require 'httparty'
-# require 'json'
+require 'roadmap'
+#require 'json'
 
 class Kele
   include HTTParty
+  include Roadmap
   # @base_uri = 'https://www.bloc.io/api/v1/'
 
   def initialize(email, password)
@@ -20,6 +22,16 @@ class Kele
     response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: @headers)
     @mentor_availability = JSON.parse(response.body)
   end
+
+  # def get_roadmap(roadmap_id)
+  #   response = self.class.get("https://www.bloc.io/api/v1/roadmaps/#{roadmap_id}", headers: @headers)
+  #   @roadmap_track = JSON.parse(response.body)
+  # end
+  #
+  # def get_checkpoint(checkpoint_id)
+  #   response = self.class.get("https://www.bloc.io/api/v1/checkpoints/#{checkpoint_id}", headers: @headers)
+  #   @checkpoint_track = JSON.parse(response.body)
+  # end
 
   # private
   #
---
+++ lib/roadmap.rb
@@ -0,0 +1,15 @@
+require 'httparty'
+
+module Roadmap
+  include HTTParty
+
+  def get_roadmap(roadmap_id)
+    response = self.class.get("https://www.bloc.io/api/v1/roadmaps/#{roadmap_id}", headers: @headers)
+    @roadmap_track = JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get("https://www.bloc.io/api/v1/checkpoints/#{checkpoint_id}", headers: @headers)
+    @checkpoint_track = JSON.parse(response.body)
+  end
+end
