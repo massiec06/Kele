95e4c01f967bbee1df8cbe09f1bdd93c4038f5b5
Chris Massie <massiec06@gmail.com>
2017-10-18 23:55:40 -0400

    checkpoint 3 & 4 completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+# require 'json'
 
 class Kele
   include HTTParty
@@ -7,12 +8,23 @@ class Kele
   def initialize(email, password)
     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {email: email, password: password})
     @auth_token = response["auth_token"]
+    @headers = {"authorization": @auth_token}
   end
 
   def get_me
-    headers = {"authorization": @auth_token}
-    response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: headers)
+    response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: @headers)
     @user = JSON.parse(response.body)
   end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: @headers)
+    @mentor_availability = JSON.parse(response.body)
+  end
+
+  # private
+  #
+  # def api_url(endpoint)
+  #   "https://www.bloc.io/api/v1/#{endpoint}"
+  # end
 
 end
